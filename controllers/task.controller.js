e09c85d8ae1d27bcee62f26d1f7e24b5d462ec40
ubeyid oumer <ubeyidah@gmail.com>
2024-07-03 11:07:43 +0300

    get all tasks logic

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,5 +1,13 @@
+import { Tasks } from "../schema/task.schema.js";
 
-const getTasks = (req, res) => {};
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Tasks.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 const createTask = (req, res) => {};
 const updateTask = (req, res) => {};
 const deleteTask = (req, res) => {};
