df8766b8092b596e562bb3c54ee2eaf16487f507
ubeyid oumer <ubeyidah@gmail.com>
2024-07-03 11:45:42 +0300

    update task logic

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -12,12 +12,25 @@ const createTask = async (req, res) => {
   try {
     const { title } = req.body;
     const newTask = await Tasks.create({ title });
-    res.status(200).json(newTask);
+    res.status(201).json(newTask);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, isCompleted } = req.body;
+    const updatedTask = await Tasks.findOneAndUpdate(
+      { _id: id },
+      { title, isCompleted }
+    );
+    if (!updatedTask) return res.sataus(404).json({ msg: "Task not found" });
+    res.status(200).json({ msg: "Task updated successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 const deleteTask = async (req, res) => {};
 
 export { getTasks, createTask, updateTask, deleteTask };
