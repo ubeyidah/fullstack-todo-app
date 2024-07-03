bbef57b693bdbd4886779cb214d92e985eb38a39
ubeyid oumer <ubeyidah@gmail.com>
2024-07-03 11:20:21 +0300

    create new task logic

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -8,8 +8,16 @@ const getTasks = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const createTask = (req, res) => {};
-const updateTask = (req, res) => {};
-const deleteTask = (req, res) => {};
+const createTask = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const newTask = await Tasks.create({ title });
+    res.status(200).json(newTask);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+const updateTask = async (req, res) => {};
+const deleteTask = async (req, res) => {};
 
 export { getTasks, createTask, updateTask, deleteTask };
